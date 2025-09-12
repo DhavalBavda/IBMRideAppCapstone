13849038200dce6034bd6b3c07f1f78f92a01be4
SanjanaV5103 <sanjanav5103@gmail.com>
2025-09-12 12:20:08 +0530

    Razorpay api

--- Python App/wallet/admin.py
+++ Python App/wallet/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Wallet
 
-# Register your models here.
+@admin.register(Wallet)
+class WalletAdmin(admin.ModelAdmin):
+    # Fields you want visible in the list page
+    list_display = ("wallet_id", "driver_id", "total_balance", "is_active", "created_at", "updated_at")
+
+    # Optional: make some fields clickable links
+    list_display_links = ("wallet_id", "driver_id")
+
+    # Optional: filters on the right side
+    list_filter = ("is_active", "created_at")
+
+    # Optional: search bar on top
+    search_fields = ("wallet_id", "driver_id")
