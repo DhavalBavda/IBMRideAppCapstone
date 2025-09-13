5cadd6ae1067aee260fcfddeaab3ae3389215051
ArjunPraja <prajapatiarjun2801@gmail.com>
2025-09-13 22:37:33 +0530

    Bug Fix

--- Python App/Admin/urls.py
+++ Python App/Admin/urls.py
@@ -1,7 +1,7 @@
 
 from django.urls import path
 
-from .views import Hello,FareCalculation, RegisterAdminView, LoginAdminView, AdminDetailView,AddOtherAdminView
+from .views import Hello,FareCalculation, RegisterAdminView, LoginAdminView, AdminDetailView
 
 
 
